e0ed0666483c8e1c85b822ada30471740a3077c4
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-02 22:51:17 +0200

    Successfully added images folder , incorp'd css

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,13 @@ function App() {
 
   return (
     <BrowserRouter>
+      <header>
+      <Link to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+    
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
