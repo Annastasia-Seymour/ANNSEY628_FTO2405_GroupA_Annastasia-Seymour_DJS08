a37003b3b0a23508a32dfc587837b2fb0493c7eb
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-04 22:56:00 +0200

    contact api to fetch requests, css done

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import '../server.js';
 
 export default function Vans(){
-    return (
-        <div className="home-container">
-            <h1>Vans page goes here</h1>
-           
-            <Link to="vans">Find your van</Link>
-        </div>
-    )
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())//converts or interprets the api to readable js
+            .then(data => setVans(data.vans))
+    }, [])
 
+    const vanElements = vans.map(van => (//map method added to create new array
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    ))
+    return (
+        <div className="van-list-container">
+        <div className="van-list">
+            {vanElements}
+        </div>
+    </div>)
+    
 }
