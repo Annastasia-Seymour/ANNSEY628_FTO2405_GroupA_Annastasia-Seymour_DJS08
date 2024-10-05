5cb1ff976b2296340365838045972eed552e5433
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-05 22:28:12 +0200

    made use of useParams hook

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,5 +1,8 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
+    const params = useParams()// had a typo here gave klomp errors
+    console.log(params)
     return <h1>Van detail page goes here</h1>
 }
--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -6,22 +6,28 @@ export default function Vans(){
     const [vans, setVans] = React.useState([])
     React.useEffect(() => {
         fetch("/api/vans")
-            .then(res => res.json())//converts or interprets the api to readable js
+            .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
 
+
     const vanElements = vans.map(van => (//map method added to create new array
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link className="link-button" to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                 <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+            
         </div>
     ))
+
     return (
         <div className="van-list-container">
+          {/* <Link className="link-button" to="/vans/${van-id-here}">Explore our vans</Link>  */}
             <h1>Explore our van options</h1>
         <div className="van-list">
             {vanElements}
