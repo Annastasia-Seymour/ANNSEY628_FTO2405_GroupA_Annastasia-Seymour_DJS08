3b43ea147c05e37e1fa8d095f1da62b39919ecb2
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-06 00:06:26 +0200

    Nested Routes explained and added

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import '../server.js';
+import '../../server.js';
 
 export default function Vans(){
     const [vans, setVans] = React.useState([])
--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,13 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter , Routes, Route, Link } from 'react-router-dom'
 import Home from "../pages/Home.jsx"
 import About from ".././pages/About.jsx"
-import Vans from '../pages/Vans.jsx'
+import Vans from '../pages/Vans/Vans.jsx'
 import "../server.js"
-import VanDetail from "../pages/VanDetail.jsx"
+import VanDetail from "../pages/Vans/VanDetail.jsx"
 import Layout from "../components/Layout.jsx"
+import Dashboard from '../pages/Host/Dashboard.jsx'
+import Income from '../pages/Host/Income.jsx'
+import Reviews from '../pages/Host/Reviews.jsx'
 
 
 
@@ -36,6 +39,11 @@ function App() {
           <Route path="/about" element={<About />}/>
           <Route path="/vans" element={<Vans />}/>
           <Route path="/vans/:id" element={<VanDetail />}/>
+          
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
       </BrowserRouter>
